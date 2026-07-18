d2043269de74eba423e54697f9a97fde982d41f2
Waleed Ahmed <wa9345253@gmail.com>
2026-07-18 20:17:58 +0500

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -19,7 +19,7 @@ export default function App() {
   const handleSubmit = async () => {
     setLoading(true)
     try {
-      const response = await fetch("http://127.0.0.1:8000/predict", {
+      const response = await fetch("https://insurance-cost-prediction-o4vl.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
